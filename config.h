a33da96df55f24484653d5f5adefa03982c53159
Richard Huang <richardhuang.huang@gmail.com>
2020-05-27 16:10:39 -0700

    make nord themed

--- config.h
+++ config.h
@@ -2,22 +2,22 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "hack:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#2e3440";
+static const char col_gray2[]       = "#434c5e";
+static const char col_gray3[]       = "#e5e9f0";
+static const char col_gray4[]       = "#d8dee9";
+static const char col_cyan[]        = "#88c0d0";
 static const char *colors[][3]      = {
   /*               fg         bg         border   */
   [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-  [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+  [SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
 };
 
 /* tagging */
@@ -75,7 +75,7 @@ static const char *mmprev[] = { "playerctl", "previous",   NULL };
 
 static Key keys[] = {
   /* modifier                     key                      function        argument */
-  { MODKEY,                       XK_p,                    spawn,          {.v = menucmd } },
+  { MODKEY,                       XK_p,                    spawn,          {.v = dmenucmd } },
   { MODKEY|ShiftMask,             XK_Return,               spawn,          {.v = termcmd } },
   { MODKEY,                       XK_b,                    togglebar,      {0} },
   { MODKEY,                       XK_j,                    focusstack,     {.i = +1 } },
