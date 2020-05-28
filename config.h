0cab5686be7c68df77e9ec8e882f541cecc8041b
Richard Huang <richardhuang.huang@gmail.com>
2020-05-27 18:35:52 -0700

    fix dmenu selected font color

--- config.h
+++ config.h
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray1, NULL };
 static const char *menucmd[]  = { "rofi", "-show", NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 /* volume */
