1c711a137b6e57f3ae4f759d46bc979e3caa655a
Richard Huang <richardhuang.huang@gmail.com>
2020-05-28 17:29:23 -0700

    move statusbar to bottom

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "hack:size=10" };
 static const char dmenufont[]       = "hack:size=10";
 static const char col_gray1[]       = "#2e3440";
